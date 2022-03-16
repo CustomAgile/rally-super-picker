9d445ee8f33388835ba58e5faf81e80f2e26042b
Charles Ferentchak <charles@customagile.com>
2022-03-16 13:50:49 -0500

    Init

---
+++ release-burnup/src/javascript/app.js
@@ -0,0 +1,222 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    setting_key: 'rally.techservices.projectselection',
+    project_names: [],
+    tag_oids: [],
+    logger: new Rally.technicalservices.Logger(),
+    defaults: {
+        padding: 5,
+        margin: 5
+    },
+    items: [
+        {xtype:'container',itemId:'message_box',tpl:'<tpl>{msg}</tpl>'},
+        {xtype:'container',layout:{type:'hbox'},defaults: { margin: 5 }, items:[
+            {xtype:'container',itemId:'release_box'},
+            {xtype:'container',itemId:'tag_box'}
+        ]},
+        {xtype:'container',itemId:'chart_box'},
+        {xtype:'tsinfolink'}
+    ],
+    launch: function() {
+        this.down('#message_box').update({msg:'Waiting for settings...'});
+        this.subscribe(this, this.setting_key, this._onSettingUpdate, this);
+        if ( ! window.top.Rally.alm ) {
+            this._addSelectors([this.getContext().getProject()]);
+        }
+    },
+    _onSettingUpdate: function(selected_projects) {
+        var me = this;
+        this.logger.log("received update ",selected_projects);
+        this.project_names = [];
+        Ext.Array.each(selected_projects,function(project){
+            me.project_names.push(project.get("Name"));
+        });
+        this._addSelectors(selected_projects);
+        //this.down('#message_box').update({msg: me.project_names.join(', ')});
+    },
+    _addSelectors: function(selected_projects){
+        this.logger.log("_addSelectors");
+        this.tag_oids = [];
+        this.logger.log("adding tag picker");
+        var tag_picker = this._addTagBox();
+        
+        this.logger.log("adding release_picker");
+        var release_picker = this._addReleaseBox(selected_projects);
+        
+        // adding change after the fact because we don't want it to fire
+        // before both choosers exist
+        tag_picker.on('blur',function(tb) {
+                var me = this;
+                this.logger.log("blur tags",tb.getValue());
+                this.tag_oids = [];
+                if ( this.chart ) { this.chart.destroy(); }
+                Ext.Array.each(tb.getValue(), function(tag){
+                    me.tag_oids.push(tag.get('ObjectID'));
+                });
+                this._getScopedReleases(release_picker.getRecord(),selected_projects);
+            },
+            this
+        );
+        
+        release_picker.on('change',function(rb,new_value,old_value) {
+                this.logger.log("change",rb.getRecord());
+                if ( this.chart ) { this.chart.destroy(); }
+                this._getScopedReleases(rb.getRecord(),selected_projects);
+            },
+            this
+        );
+        
+        if ( release_picker.getRecord() ) {
+            this._getScopedReleases(release_picker.getRecord(),selected_projects);
+        }
+        
+    },
+    _addTagBox: function() {
+        this.down('#tag_box').removeAll();
+        return this.down('#tag_box').add({
+            xtype: 'rallytagpicker',
+            autoExpand: false,
+            labelWidth: 35,
+            fieldLabel: 'Tag(s)',
+            stateId:'ts_super_releaseburndown_tags',
+            stateEvents:'change',
+            stateful: true
+        });
+    },
+    _addReleaseBox: function(selected_projects) {
+        this.down('#release_box').removeAll();
+        this.down('#message_box').update();
+        
+        return this.down('#release_box').add({
+            xtype:'rallyreleasecombobox',
+            fieldLabel:'Release:',
+            labelWidth: 45,
+            stateId:'ts_super_releaseburndown_release',
+            stateEvents:'change',
+            stateful: true
+        });
+    },
+    _getScopedReleases: function(release,selected_projects) {        
+        var filter = Ext.create('Rally.data.wsapi.Filter',{property:"Name",value:release.get('Name')});
+        
+        var project_filter = null;
+        if ( selected_projects.length > 0 ) {
+            var project_oid = selected_projects[0].ObjectID;
+            if ( !project_oid  ) {
+                project_oid = selected_projects[0].get('ObjectID')
+            }
+            project_filter = Ext.create('Rally.data.wsapi.Filter',{
+                property:"Project.ObjectID",
+                value:project_oid
+            });
+        
+            for ( var i=1;i<selected_projects.length;i++ ) {
+                project_filter = project_filter.or(
+                    Ext.create('Rally.data.wsapi.Filter',{property:"Project.ObjectID",value:selected_projects[i].get('ObjectID')})
+                );
+            }
+        }
+        
+        if ( project_filter ) { 
+            filter = filter.and(project_filter);
+        }
+        
+        Ext.create('Rally.data.wsapi.Store',{
+            model:'Release',
+            autoLoad: true,
+            filters:filter,
+            context: {
+                projectScopeUp: false,
+                projectScopeDown: false,
+                project: null
+            },
+            listeners: {
+                scope: this,
+                load: function(store,records) {
+                    var record_oids = [];
+                    Ext.Array.each(records,function(record){
+                        record_oids.push(record.get('ObjectID'));
+                    });
+                    this._getReleaseData(release,record_oids,selected_projects);
+                }
+            }
+        });
+    },
+    _getReleaseData:function(release,release_oids,selected_projects) {
+        this.logger.log(release_oids,selected_projects);
+        var start_date = release.get("ReleaseStartDate");
+        var end_date = release.get("ReleaseDate");
+        this.logger.log(release.get("Name"),start_date,end_date);
+        
+        if ( this.chart ) { this.chart.destroy(); }
+
+        var project_names = this.project_names.join(',');
+        var filters = [
+            { property:'_TypeHierarchy',operator:'in',value:['HierarchicalRequirement'] },
+            { property:'Release',operator:'in',value:release_oids }
+        ];
+        if ( this.tag_oids.length > 0 ) {
+            filters.push({property:'Tags',operator:'in',value:this.tag_oids});
+        }
+        this.chart = Ext.create('Rally.ui.chart.Chart', {
+            storeType: 'Rally.data.lookback.SnapshotStore',
+            calculatorType: 'Rally.technicalservices.calculator.BurnUp',
+            calculatorConfig: {
+                endDate: end_date,
+                startDate: start_date
+            },
+            storeConfig: {
+               filters: filters,
+               fetch: ['ScheduleState', 'PlanEstimate'],
+               hydrate: ['ScheduleState'],
+               sort: {
+                   '_ValidFrom': 1
+               }
+            },
+            chartConfig: {
+                title: {
+                    text: project_names
+                },
+                chart: {
+                    zoomType: "xy"
+                },
+                xAxis: {
+                    title: {
+                        text: "Date"
+                    },
+                    labels: {
+                        align: 'left',
+                        rotation: 75
+                    }
+                },
+                yAxis: [
+                    {
+                        title: {
+                            text: "Points"
+                        }
+                    }
+                ],
+                plotOptions: {
+                    line: {
+                        color: "#000"
+                    },
+                    column: {
+                        stacking: null,
+                        color: "#6AB17D",
+                        lineColor: "#666666",
+                        lineWidth: 1,
+                        marker: {
+                            lineWidth: 1,
+                            lineColor: "#666666"
+                        },
+                        shadow: false
+                    }
+                }
+            }
+        });
+        
+        this.down('#chart_box').add(this.chart);
+
+    }
+});
---
+++ super-picker/src/javascript/app.js
@@ -0,0 +1,237 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    type_key: 'rally.techservices.projecttype',
+    setting_key: 'rally.techservices.projectselection',
+    type_field_name: '_credittype',
+    logger: new Rally.technicalservices.Logger(),
+    items: [
+        {xtype:'container',itemId:'outer_box', autoScroll: true, height: 400, defaults: { margin: 5, padding: 5 }, layout: 'hbox', items: [
+            {xtype:'container',itemId:'source_box',  width: 150},
+            {xtype:'container',itemId:'target_box',  width: 150}
+        ]},
+        {xtype:'tsinfolink'}
+    ],
+    launch: function() {
+        var me = this;
+        Deft.Chain.pipeline([this._getProjects,this._getProjectTypes,this._getExistingSettings,this._displayProjects],this);
+    },
+    _getProjects: function() {
+        var deferred = Ext.create('Deft.Deferred');
+        Ext.create('Rally.data.wsapi.Store',{
+            model:'Project',
+            autoLoad: true,
+            fetch:['ObjectID','Children','Name'],
+            limit:'Infinity',
+            listeners: {
+                load: function(store,projects) {
+                    var leaf_projects = [];
+                    Ext.Array.each(projects,function(project){
+                        if ( project.get('Children').Count == 0 ) {
+                            leaf_projects.push(project);
+                        }
+                    });
+                    deferred.resolve(leaf_projects);
+                }
+            }
+        });
+        return deferred.promise;
+    },
+    _getProjectTypes: function(projects){
+        var me = this;
+        this.logger.log("_getProjectTypes",projects);
+        var deferred = Ext.create('Deft.Deferred');
+        var project_hash = {}; // key will be oid
+        var filtered_projects = [];
+        Ext.Array.each(projects,function(project){
+            project.set(me.type_field_name,"None");
+            project_hash[project.get('ObjectID')] = project;
+        });
+        
+        // this is for development ease
+        if ( this.getAppId() ) {
+            this.logger.log("inside Rally");
+            Ext.create('Rally.data.wsapi.Store',{
+                model: 'Preference',
+                autoLoad: true,
+                filters: [{property:"Name",value:this.type_key}],
+                limit:'Infinity',
+                listeners:{
+                    scope: this,
+                    load: function(store,prefs) {
+                        Ext.Array.each( prefs, function(pref){
+                            var pref_project_oid = pref.get('Project').ObjectID;
+                            if ( project_hash[pref_project_oid] ) {
+                                var project = project_hash[pref_project_oid];
+                                project.set(me.type_field_name,pref.get('Value'));
+                                if ( pref.get('Value') !== "None" ) {
+                                    filtered_projects.push(project);
+                                }
+                            }
+                        });
+                        deferred.resolve(filtered_projects);
+                    }
+                }
+            });
+        } else {
+            this.logger.log("Outside Rally");
+            var counter = 1;
+            Ext.Array.each(projects,function(project){
+                if ( counter % 2 === 0 ) {
+                    project.set(me.type_field_name,"Type A");
+                } else {
+                    project.set(me.type_field_name,"Type B");
+                }
+                counter++;
+            });
+            deferred.resolve(projects);
+        }
+        
+        return deferred.promise;
+    },
+    _getExistingSettings: function(projects) {
+        var me = this;
+        var deferred = Ext.create('Deft.Deferred');
+        Rally.data.PreferenceManager.load({
+            appID: this.getAppId(),
+            filterByUser: false,
+            success: function(prefs) {
+                var selected_oids = [];
+                if ( prefs[me.setting_key] ) {
+                    selected_oids = Ext.JSON.decode(prefs[me.setting_key]);
+                }
+                Ext.Array.each(projects,function(project){
+                    if ( Ext.Array.indexOf(selected_oids,project.get('ObjectID')) > -1 ) {
+                        me._setSelected(project, true);
+                    } else { 
+                        me._setSelected(project, false);
+                    }
+                });
+                me._sendMessage(projects);
+                deferred.resolve(projects);
+            }
+        });
+        return deferred.promise;
+    },
+    _sendMessage: function(projects) {
+        var selected_project_oids = [];
+        Ext.Array.each(projects, function(project){
+            if ( project.get("_selected") ) {
+                selected_project_oids.push(project);
+            }
+        });
+        this.publish(this.setting_key, selected_project_oids);
+    },
+    _displayProjects: function(projects){
+        var me = this;
+        this.project_set = [];
+        var source_data = [];
+        var target_data = [];
+        
+        Ext.Array.each(projects, function(project) {
+            me.project_set.push(project);
+            if ( project.get('_selected') ) {
+                target_data.push(project);
+            } else {
+                source_data.push(project);
+            }
+        });
+        
+        this.source_store = Ext.create('Rally.data.custom.Store',{
+            groupField: me.type_field_name,
+            data:source_data,
+            pageSize:3000
+        });
+        
+        var target_store = Ext.create('Rally.data.custom.Store',{
+            data:target_data
+        });   
+        
+        this.down('#target_box').add({
+            xtype:'rallygrid',
+            store: target_store,
+            showRowActionsColumn: false,
+            showPagingToolbar: false,
+            columnCfgs: [
+                { text: 'Selected Projects', dataIndex: 'Name' }
+            ],
+            viewConfig: {
+                plugins: {
+                    ptype: 'gridviewdragdrop',
+                    dragGroup: 'firstGridDDGroup',
+                    dropGroup: 'secondGridDDGroup'
+                },
+                listeners: {
+                    drop: function(node, data, dropRec, dropPosition) {
+                        me._setSelected(data.records[0], true);
+                    }
+                }
+            }
+        }); 
+        
+        this.grid = this.down('#source_box').add({
+            xtype:'rallygrid',
+            store: this.source_store,
+            features: [{
+                ftype:'grouping',
+                groupHeaderTpl: '{name}'
+            }],
+            showRowActionsColumn: false,
+            showPagingToolbar: false,
+            columnCfgs: [
+                { text: 'Available Projects', dataIndex: 'Name' }
+            ],
+            viewConfig: {
+                plugins: {
+                    ptype: 'gridviewdragdrop',
+                    dragGroup: 'secondGridDDGroup',
+                    dropGroup: 'firstGridDDGroup'
+                },
+                listeners: {
+                    drop: function(node, data, dropRec, dropPosition) {
+//                        var dropOn = dropRec ? ' ' + dropPosition + ' ' + dropRec.get('Name') : ' on empty view';
+//                        me.logger.log('dropRec',dropRec);
+//                        me.logger.log('dropPosition',dropPosition);
+//                        me.logger.log('node',node);
+//                        me.logger.log( 'Dropped ' + data.records[0].get(me.type_field_name) + dropOn);
+                        me._setSelected(data.records[0], false);
+                    }
+                }
+            }
+        }); 
+    },
+    _setSelected: function(record,selected) {
+        var me = this;
+        record.set("_selected",selected);
+        
+        if ( this.source_store ) {
+            this.logger.log("setting selected",record,record.get('ObjectID'),selected);
+//            this.source_store.clearFilter(true);
+//            var item = this.source_store.findRecord("ObjectID",record.get('ObjectID'));
+//            this.source_store.filter({property:'_selected',value:false});
+//            if ( item ) {
+//                item.set("_selected",selected);
+//            }
+        }
+        
+        var selected_array = [];
+        Ext.Array.each(this.project_set, function(project){
+            if ( project.get('_selected') ) {
+                selected_array.push(project.get('ObjectID'));
+            }
+        });
+        me._sendMessage(this.project_set);
+        me.logger.log('--', selected_array);
+        
+        var settings = {};
+        settings[this.setting_key] = Ext.JSON.encode(selected_array);
+        Rally.data.PreferenceManager.update({
+            appID: this.getAppId(),
+            filterByUser: false,
+            settings: settings,
+            success: function(updatedRecords, notUpdatedRecords) {
+                //yay!
+            }
+        });
+    }
+});
